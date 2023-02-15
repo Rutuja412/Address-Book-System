0ac626c273df091cdf02d16490956d36b3238007
Rutuja412 <rutujakadam4545@gmail.com>
2023-02-15 18:41:00 +0530

    [Rutuja]:Create a table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,8 @@
 ﻿-- Welcome to the Address Book System
 --UC1- Create database
 create database Address_Book_System
+--UC2 Create Table Address_Book
+
+create Table Address_Book
+(FirstName varchar(100), LastName varchar(100), Address varchar(300), 
+City varchar(50), State varchar(30), ZipCode int, PhoneNumber bigint, EMailId varchar(50) )
